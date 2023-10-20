40dfe719f1a2cdba8a8a9c97bde71ff48d4cdc1f
sarang_daddy <sarang_daddy@naver.com>
2023-10-21 03:52:51 +0900

    Feat : Create layout component

--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,8 +1,5 @@
 import { useState } from 'react';
-
-function cls(...classnames: string[]) {
-  return classnames.join(' ');
-}
+import { cls } from '../../libs/utils';
 
 const Enter = () => {
   const [method, setMethod] = useState<'email' | 'phone'>('email');
